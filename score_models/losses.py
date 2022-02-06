1cf2a51518a8ac8bda3771b47f08d42b704968da
Eric Ma <ericmajinglong@gmail.com>
2022-02-05 19:58:15 -0500

    First commit of working framework for score functions and model inference in 1D Gaussians.

---
+++ score_models/losses.py
@@ -0,0 +1,26 @@
+"""Implementation of loss function for score matching."""
+from functools import partial
+
+from jax import grad
+from jax import numpy as np
+from jax import vmap
+
+
+def loss(params, score_func, batch) -> float:
+    """Score matching loss function.
+
+    :param params: The parameters to the score function.
+    :param score_func: Score function with signature `func(params, batch)`,
+        which returns a scalar.
+    :param batch: A batch of data. Should be of shape (batch, :),
+        where `:` refers to at least 1 more dimension,
+        and should return a scalar float.
+        We `vmap` the `scorefunc` and `grad(scorefunc)` over the batch dimension.
+    :returns: Score matching loss, a float.
+    """
+    score_func = partial(score_func, params)
+    dscorefunc = grad(score_func)
+    term1 = vmap(dscorefunc)(batch)
+    term2 = 0.5 * vmap(score_func)(batch) ** 2
+    inner_term = term1 + term2
+    return np.mean(inner_term)
--- score_models/models.py
+++ score_models/models.py
@@ -1,1 +1,69 @@
 """Custom model code for score-models."""
+from functools import partial
+from typing import Callable, Tuple
+
+import jax.numpy as np
+from jax import grad
+from jax.example_libraries import stax
+from jax.scipy.stats import norm
+
+
+def gaussian_score_func(params: Tuple[float, float], draw: float) -> float:
+    """Gaussian score function for 1 draw.
+
+    :param params: Tuple of `mu` and `log_sigma`.
+    :param draw: One observed value.
+    :returns: Data score (conditioned on `params`).
+    """
+    mu, log_sigma = params
+    sigma = np.exp(log_sigma)
+    logp_func = partial(norm.logpdf, loc=mu, scale=sigma)
+    return grad(logp_func)(draw)
+
+
+def nn_model() -> Tuple[Callable, Callable]:
+    """Simple neural network model for predicting score from.
+
+    Example:
+
+        >>> from score_models.models import nn_model
+        >>> init_fun, apply_fun = nn_model()
+
+    :returns: (init_fun, apply_fun) pair.
+    """
+    init_fun, apply_fun = stax.serial(stax.Dense(1024), stax.Relu, stax.Dense(1))
+    return init_fun, apply_fun
+
+
+def nn_score_func(params, draw: float):
+    """Scalar version of the score function.
+
+    A hack to get around the shape issues that plague tensor math :).
+    `grad` requires the output to be a scalar, so we need to hack and squeeze scores.
+
+    `batch` should be a scalar.
+
+    Example:
+
+        >>> from score_models.models import nn_model, nn_score_func
+        >>> from jax.random import PRNGKey
+        >>> init_fun, apply_fun = nn_model()
+        >>> key = PRNGKey(44)
+        >>> _, params = init_fun(key, input_shape=(None, 1))
+        >>> nn_score_func(params, 0.5)
+        DeviceArray(-0.01079839, dtype=float32)
+
+    :param params: Parameters generated from `nn_model()`'s initialization function.
+    :param draw: One observed value.
+    :return: Predicted score, given parameters.
+    """
+    _, apply_fun = nn_model()
+    # The reshape(-1, 1) is necessary to interface with stax' Dense layers.
+    apply_fun = partial(apply_fun, params)
+    draw = np.reshape(draw, (-1, 1))
+    scores = apply_fun(draw).squeeze()
+    return scores
+
+
+# def score_func_haiku(params, batch):
+#     pass
