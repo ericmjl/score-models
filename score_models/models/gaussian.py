bfa9294f35a70c135fa491782113885f899359ac
Eric Ma <ericmajinglong@gmail.com>
2022-04-18 23:29:52 -0400

    Fix GaussianModel to use np.array instead of raw floats.

--- score_models/models/gaussian.py
+++ score_models/models/gaussian.py
@@ -5,10 +5,10 @@ from jax.scipy.stats import norm
 
 
 class GaussianModel(eqx.Module):
-    mu: float = 0.0
-    log_sigma: float = 0.0
+    mu: np.array = np.array(0.0)
+    log_sigma: np.array = np.array(0.0)
 
-    @jit
+    @eqx.filter_jit
     def __call__(self, x):
         gaussian_score_func = jacfwd(norm.logpdf)
         return gaussian_score_func(x, loc=self.mu, scale=np.exp(self.log_sigma))
