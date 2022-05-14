a8c4e88548662f83038a619091c6ac3821df4e1f
Eric Ma <ericmajinglong@gmail.com>
2022-05-14 17:40:34 -0400

    Add SDEGaussian into models.py

--- score_models/models/__init__.py
+++ score_models/models/__init__.py
@@ -1,5 +1,5 @@
 """Top-level models API."""
 from .feedforward import FeedForwardModel1D
-from .gaussian import GaussianModel
+from .gaussian import GaussianModel, SDEGaussianModel
 
-__all__ = ["FeedForwardModel1D", "GaussianModel"]
+__all__ = ["FeedForwardModel1D", "GaussianModel", "SDEGaussianModel"]
